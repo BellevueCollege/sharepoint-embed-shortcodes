5ecfc7de5e4864b5cecd926349e5b68c1b6a4f9f
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2017-07-27 15:10:35 -0700

    Code to support Excel embedding

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# sharepoint_embed_shortcodes
+# SharePoint Embed Shortcodes
 Embed publicly shared documents from SharePoint and OneDrive for Business in WordPress
+
+Currently shortcodes are only provided to embed Excel documents; Word and PowerPoint documents should be supported,
+but are waiting on Microsoft to provide documentation.
+
+## Excel Shortcode Documentation
+The Excel embed shortcode is `[sp-excel-embed]`. It accepts the following attributes:
+
+* `source` : accepts public sharing URL (default: false)
+* `width` : pixel or precentage value (default: 100%)
+* `height` : pixel or precentage value (default: 500px)
+* `bifeatures` : enable BI features such as Power View visualizations, PivotTables, and Data Model-based slicers work in the embedded workbook (default: true)
+* `interactive` : enable interactivity with filters and Pivot tables in the workbook (default:false)
+* `download` : show integrated download link
+
+Example shortocde with default values:
+```
+[sp-excel-embed source="https://bellevuec.sharepoint.com/sites/its/web/_layouts/15/guestaccess.aspx?docid=XXXXX&authkey=XXXXXX" width="100%" height="500px" bifeatures="true" interactive="false" download="true"]
+```
+
+### How to get source URL
+SharePoint and OneDrive for Business allow you to create sharing links in a variety of ways; any of these will work as long as the settings are correct. 
+
+Primary method:
+1. Start from the Excel Online 'read only' view
+2. Click **Share** then **Share with people**
+3. Click **Get a link** 
+4. Select **View Link - no sign-in required**. Warning: Make sure that the link does **NOT** allow for edit access!
+5. Select and copy the link provided to the clipboard
+6. Click **Close**
+
+## Microsoft Documentation
+See this [Office Support Article](https://support.office.com/en-us/article/Embed-your-Excel-workbook-on-your-web-page-or-blog-from-SharePoint-or-OneDrive-for-Business-7af74ce6-e8a0-48ac-ba3b-a1dd627b7773) for documentation on how to configure an Excel embed. 
---
+++ sharepoint-embed-shortcodes.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: SharePoint Document Embed
+Plugin URI: https://github.com/BellevueCollege/
+Description: Embed publicly shared documents from SharePoint and OneDrive for Business in WordPress
+Author: Bellevue College Integration Team
+Version: 0.0.0.1
+Author URI: http://www.bellevuecollege.edu
+*/
+
+defined( 'ABSPATH' ) || exit;
+
+// Shortcode Setup
+function spembed_excel_shortcode( $sc_config ) {
+
+	// Shortcode Attributes
+	$sc_config = shortcode_atts( array(
+		'source'      => false,
+		'width'       => '100%',
+		'height'      => '500px',
+		'bifeatures'  => true,
+		'interactive' => false,
+		'download'    => true,
+
+	), $sc_config, 'spembed_excel_shortcode' );
+
+	// Allowed hostnames
+	$allowed_hosts = array(
+		'bellevuec.sharepoint.com',
+		'bellevuec-my.sharepoint.com',
+	);
+
+	// Parse URL to validate it is from one of the allowed hosts
+	$parsed_source_url = wp_parse_url( $sc_config['source'] );
+	if ( in_array( $parsed_source_url['host'], $allowed_hosts, true ) ) {
+
+		// Build Request URL
+		$request_url = $sc_config['source'] .
+			'&action=embedview&wdbipreview=' . $sc_config['bifeatures'] .
+			'&wdAllowInteractivity=' . $sc_config['interactive'] .
+			'&wdAllowInteractivity=' . $sc_config['interactive'] .
+			'&wdDownloadButton=' . $sc_config['download'];
+
+		// Attempt to hide the download link if perameter set
+		if ( $sc_config['download'] ) {
+			$download_link = '<small><a href="' . $sc_config['source'] . '" target="_blank">Open this spreadsheet in a new window <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span></a></small>';
+		} else {
+			$download_link = '<a class="sr-only" href="' . $sc_config['source'] . '" target="_blank">Open this spreadsheet in a new window</a>';
+		}
+
+		// Return iFrame with embedded document
+		return '<iframe
+			aria-hidden="true" 
+			name="Embedded Excel Document" 
+			width="' . $sc_config['width'] . '"
+			height="' . $sc_config['height'] . '"
+			scrolling="no" 
+			src="' . esc_url( $request_url ) . '"></iframe>
+			' . $download_link;
+	} else {
+
+		// Return error message
+		return '<p class="alert alert-danger"><strong>This document can not be displayed</strong> It doesn\'t look like this document is on Bellevue College\'s SharePoint or OneDrive for Business, and it can not be displayed.</p>';
+	}
+}
+
+add_shortcode( 'sp-excel-embed', 'spembed_excel_shortcode' );
